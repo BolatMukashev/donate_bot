01f717c72814b258603bcbdee46d89b80188a6c6
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2025-10-20 14:48:08 +0500

    исправил получение переменных

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
+import os
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-BOT_API_KEY = config.get("BOT_API_KEY")
-ADMIN_ID = int(config.get("ADMIN_ID"))
+BOT_API_KEY = os.environ.get("BOT_API_KEY") or config.get("BOT_API_KEY")
+ADMIN_ID = int(os.environ.get("ADMIN_ID")) if os.environ.get("ADMIN_ID") else int(config.get("ADMIN_ID"))
 
 
-YDB_ENDPOINT = config.get("YDB_ENDPOINT")
-YDB_PATH = config.get("YDB_PATH")
-YDB_TOKEN = config.get("YDB_TOKEN")
+YDB_ENDPOINT = os.environ.get("YDB_ENDPOINT") or config.get("YDB_ENDPOINT")
+YDB_PATH = os.environ.get("YDB_PATH") or config.get("YDB_PATH")
+YDB_TOKEN = os.environ.get("YDB_TOKEN") or config.get("YDB_TOKEN")
 
 
 START_IMAGE = 'AgACAgIAAxkBAAMYaPNyloVz7bI3vAqvG23N_xgKHCMAAmj4MRtVnqBLipazNxMXVDwBAAMCAAN5AAM2BA'
